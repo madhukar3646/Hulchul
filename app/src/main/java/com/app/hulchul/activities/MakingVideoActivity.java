09faf51ecb473cc67c1b250b42303c0eee5105a7
madhukar3646 <madhukar3646@gmail.com>
2019-03-22 17:59:36 +0530

    latest changes

--- app/src/main/java/com/app/hulchul/activities/MakingVideoActivity.java
+++ app/src/main/java/com/app/hulchul/activities/MakingVideoActivity.java
@@ -8,7 +8,6 @@ import android.net.Uri;
 import android.os.Environment;
 import android.os.Handler;
 import android.provider.MediaStore;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
